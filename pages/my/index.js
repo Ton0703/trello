f1c41cd1ab81440bf7874c8e7d30b00d559e4c7f
Ton0703 <1120367213@qq.com>
2020-07-06 15:59:09 +0800

    sass config

---
+++ pages/my/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.scss'
+
+function My() {
+    return (
+        <div className='my'>
+            <div className="one">Ton</div>
+            BUU
+        </div>
+    )
+}
+
+export default My
